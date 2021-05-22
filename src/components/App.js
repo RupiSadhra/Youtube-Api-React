2a49d77821f0df0bb97924983d29a55dab91527a
Rupi Sadhra <varinderjitkaur@developersinstitute.co.nz>
2021-05-23 10:45:30 +1200

    youtube api implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
-const App = () => {
-  return (
-    <div className="ui container">
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  state = { videos: [] };
+  onTermSubmit = (term) => {
+    console.log(term);
+    youtube
+      .get("/search", { params: { q: term } })
+      .then((result) => {
+        this.setState({ videos: result.data.items });
+      })
+      .catch((error) => {
+        console.log(`Youtube api error... ${error}`);
+      });
+  };
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 class SearchBar extends React.Component {
   state = { term: "cooking" };
@@ -10,16 +11,19 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div className="ui segment search-bar">
-        <form className="ui form">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            this.props.onTermSubmit(this.state.term);
+          }}
+          className="ui form"
+        >
           <div className="field">
             <label>Video Search</label>
             <input
               type="text"
               value={this.state.term}
               onChange={this.onInputChange}
-              onSubmit={(e) => {
-                e.preventDefault();
-              }}
             />
           </div>
         </form>
