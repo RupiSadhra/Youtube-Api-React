a287019293b692d3480d52aec3e6b6f8bca62322
Rupi Sadhra <varinderjitkaur@developersinstitute.co.nz>
2021-05-23 12:34:20 +1200

    Video Player component implemented

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,31 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-  state = { videos: [], selectedVideo: {} };
+  state = { videos: [], selectedVideo: null };
+  componentDidMount() {
+    youtube
+      .get("/search", { params: { q: "new zealand latest news" } })
+      .then((result) => {
+        this.setState({
+          videos: result.data.items,
+          selectedVideo: result.data.items[0],
+        });
+      })
+      .catch((error) => {
+        console.log(`Youtube api error... ${error}`);
+      });
+  }
   onTermSubmit = (term) => {
-    console.log(term);
     youtube
       .get("/search", { params: { q: term } })
       .then((result) => {
-        this.setState({ videos: result.data.items });
+        this.setState({
+          videos: result.data.items,
+          selectedVideo: result.data.items[0],
+        });
       })
       .catch((error) => {
         console.log(`Youtube api error... ${error}`);
@@ -18,17 +34,25 @@ class App extends React.Component {
   };
 
   onVideoSelect = (video) => {
-    console.log("From App:", video);
     this.setState({ selectedVideo: video });
   };
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
-        <VideoList
-          onVideoSelect={this.onVideoSelect}
-          videos={this.state.videos}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
